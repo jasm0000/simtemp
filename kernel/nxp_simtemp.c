de02720eb6f7627610fd1a2959d80cafa2e32836
Jorge Sánchez <jorgsanc@yahoo.com>
2025-10-04 22:08:59 -0600

    Setup timer: cleanup

--- kernel/nxp_simtemp.c
+++ kernel/nxp_simtemp.c
@@ -6,31 +6,49 @@
 #include <linux/uaccess.h>
 #include <linux/workqueue.h>
 
+
+/************************ MACROS DEFINITIONS **************************/
+
+#define VERSION "b001"
+
+
+/************************ GLOBAL VARIABLES ****************************/
+
+char arreglito[20] = "0123456789ABCDEFG\n";
+struct delayed_work readSampleTimer;
+
+/*********************************************************************/
+
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jorge Sanchez");
 MODULE_DESCRIPTION("miscdevice /dev/simtemp");
-MODULE_VERSION("0.1");
+MODULE_VERSION(VERSION);
 
-// --- Operaciones mínimas del char device ---
 
-char arreglito[20] = "0123456789ABCDEFG\n";
 
-struct delayed_work readSampleTimer;
 
+/*********************************************************************/
+/********************  TIMER FUNCTIONS *******************************/
+/*********************************************************************/
+
+/********************  TIMER RESET *******************************/
 
 static void timerReset(void)
 {
     schedule_delayed_work(&readSampleTimer, 2 * HZ);
 }
 
+/******************** TIMER CALL BACK ****************************/
+
 static void readSampleTimerCallback(struct work_struct *work)
 {
     static char simCnt = 0;
-    printk(KERN_INFO "Timer ejecutado, generando muestra..., %i\n",simCnt++);
+    printk(KERN_INFO "Timer ejecutado, muestra numero: %i\n",simCnt++);
     timerReset();
 }
 
-
+/******************** TIMER INIT ********************************/
 
 static void timerInit(void)
 {
@@ -38,90 +56,80 @@ static void timerInit(void)
     timerReset();
 }
 
-
+/******************* TIMER DE INIT ******************************/
 
 static void timerDeInit(void)
 {
     cancel_delayed_work_sync(&readSampleTimer);
 }
 
+/*********************************************************************/
+/*********************************************************************/
+/********************  KERNEL DEVICE SETUP ***************************/
+/*********************************************************************/
+/*********************************************************************/
 
 
-static ssize_t simtemp_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+/******************* DEVICE READ OPERATION **********************/
+
+
+static ssize_t simtempRead(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
-    // Por ahora no hay datos; regresamos "intenta de nuevo"
-    copy_to_user(buf, arreglito, 17);
-
-    return 17;
-
-    buf[0] = arreglito[0];
-    buf[1] = arreglito[1];
-    buf[2] = arreglito[2];
-    buf[3] = arreglito[3];
-    buf[4] = arreglito[4];
-    buf[5] = arreglito[5];
-    buf[6] = arreglito[6];
-    buf[7] = arreglito[7];
-    buf[8] = arreglito[8];
-    buf[9] = arreglito[9];
-    buf[10] = arreglito[10];
-    buf[11] = arreglito[011];
-    buf[12] = arreglito[012];
-    buf[13] = arreglito[013];
-    buf[14] = arreglito[014];
-    buf[15] = arreglito[015];
-    buf[16] = arreglito[016];
-    buf[17] = arreglito[017];
-
-//    return -EAGAIN;
+    return -EAGAIN;
 }
 
-static int simtemp_open(struct inode *ino, struct file *f)
+/******************* DEVICE OPEN OPERATION ***************************/
+
+static int simtempOpen(struct inode *ino, struct file *f)
 {
     pr_info("simtemp: open\n");
     return 0;
 }
 
-static int simtemp_release(struct inode *ino, struct file *f)
+/******************* DEVICE RELEASE OPERATION ************************/
+
+
+static int simtempRelease(struct inode *ino, struct file *f)
 {
     pr_info("simtemp: release\n");
     return 0;
 }
 
 // Tabla de operaciones
-static const struct file_operations simtemp_fops = {
+static const struct file_operations simtempFOps = {
     .owner   = THIS_MODULE,
-    .read    = simtemp_read,
-    .open    = simtemp_open,
-    .release = simtemp_release,
+    .read    = simtempRead,
+    .open    = simtempOpen,
+    .release = simtempRelease,
 };
 
 // miscdevice crea /dev/simtemp automáticamente
-static struct miscdevice simtemp_miscdev = {
+static struct miscdevice simtempMiscDev = {
     .minor = MISC_DYNAMIC_MINOR,
-    .name  = "simtemp",
-    .fops  = &simtemp_fops,
-    .mode  = 0666,
+    .name  = "Temperature simulator",
+    .fops  = &simtempFOps,
+    .mode  = 0666,     //  Read write permissions for everybody
 };
 
-static int __init simtemp_init(void)
+static int __init simtempInit(void)
 {
-    int ret = misc_register(&simtemp_miscdev);
-    if (ret) {
-        pr_err("simtemp: misc_register falló (%d)\n", ret);
-        return ret;
+    int retVal = misc_register(&simtempMiscDev);
+    if (retVal) 
+    {
+        pr_err("simtemp: misc_register failed (%d)\n", retVal);
+        return retVal;
     }
     timerInit();
-    pr_info("simtemp: cargado. /dev/simtemp listo (v0.1)\n");
+    pr_info("simtemp: Loaded. /dev/simtemp Ready "VERSION"\n");
     return 0;
 }
 
-static void __exit simtemp_exit(void)
+static void __exit simtempExit(void)
 {
     timerDeInit();
-    misc_deregister(&simtemp_miscdev);
-    pr_info("simtemp: descargado.\n");
+    misc_deregister(&simtempMiscDev);
+    pr_info("simtemp: Unloaded.\n");
 }
 
-module_init(simtemp_init);
-module_exit(simtemp_exit);
+module_init(simtempInit);
+module_exit(simtempExit);
