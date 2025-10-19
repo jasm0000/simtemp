4c464806eb271c83ec75f5d78e8a8c030e9e1d6a
Jorge Sánchez <jorgsanc@yahoo.com>
2025-10-19 15:14:41 -0600

    CLI: Poll, read once, first approach

--- user/cli/main.cpp
+++ user/cli/main.cpp
@@ -1,6 +1,36 @@
 // simtemp/user/cli/main.cpp
 #include <iostream>
 #include <string>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <cstring>
+#include <poll.h>
+
+using namespace std;
+
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************** GLOBAL CONSTANTS ****************************/
+/****************************************************************************/
+/****************************************************************************/
+
+static const char* devPath = "/dev/simtemp";
+
+enum commandType
+{
+   CMD_NONE,
+   CMD_ONCE
+};
+
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************** GLOBAL VARIABLES ****************************/
+/****************************************************************************/
+/****************************************************************************/
+
 
 
 /****************************************************************************/
@@ -11,9 +41,64 @@ static void printHelp()
 {
    std::cout << "simtempCLI (hello)\n";
    std::cout << "Usage:\n";
-   std::cout << "  ./simtempCLI               # Hello world\n";
-   std::cout << "  ./simtempCLI -help        # Show this help screen\n";
+   std::cout << "   ./simtempCli --once [--nonblock] [--timeout MS]\n";
 }
+
+/****************************************************************************/
+/********************************  ONCE  *********************************/
+/****************************************************************************/
+
+static bool readOnce(bool nonblock, int timeoutMs)
+{
+   int flags = O_RDONLY;
+   if (nonblock)
+   {
+      flags |= O_NONBLOCK;
+   }
+
+   int fd = open(devPath, flags);
+   if (fd < 0)
+   {
+      cerr << "ERROR: Could not open " << devPath << " (" << strerror(errno) << ")\n";
+      return false;
+   }
+
+   // Using poll to wait for data from driver
+   struct pollfd pfd;
+   pfd.fd = fd;
+   pfd.events = POLLIN | POLLRDNORM | POLLPRI;
+
+   int ret = poll(&pfd, 1, timeoutMs);
+   if (ret < 0)
+   {
+      cerr << "ERROR: poll failed (" << strerror(errno) << ")\n";
+      close(fd);
+      return false;
+   }
+   if (0 == ret)
+   {
+      cerr << "TIMEOUT: no data received in " << timeoutMs << " ms\n";
+      close(fd);
+      return false;
+   }
+
+   // Reading what driver has to return
+   char buffer[128] = {0};
+   ssize_t bytesRead = read(fd, buffer, sizeof(buffer) - 1);
+   if (bytesRead < 0)
+   {
+      cerr << "ERROR: read failed (" << strerror(errno) << ")\n";
+      close(fd);
+      return false;
+   }
+
+   cout << "Read from driver (" << bytesRead << " bytes):\n";
+   cout << buffer << endl;
+
+   close(fd);
+   return true;
+}
+
 
 /****************************************************************************/
 /****************************************************************************/
@@ -24,16 +109,45 @@ static void printHelp()
 
 int main(int argc, char** argv)
 {
-   if (argc > 1)
+   int timeoutMs = -1;
+   bool nonblock = false;
+
+   enum commandType command = CMD_NONE;
+
+   /*  PARSE INPUT PARAMETERS */
+   for (int i = 1; i < argc; ++i)
    {
-      std::string arg = argv[1];
-      if (arg == "-help" || arg == "-h")
+      string arg = argv[i];
+      if (arg == "--help" || arg == "--h")
       {
          printHelp();
          return 0;
       }
+      else if (arg == "--once")
+      {
+         command = CMD_ONCE;
+
+      }
+      else
+      {
+         cerr << "Unknown parameters: " << arg << endl;
+         printHelp();
+         return 1;
+      }
    }
 
-   std::cout << "simtemp CLI: Hello world!\n";
+   switch (command)
+   {
+      case CMD_ONCE:
+         if (!readOnce(nonblock, timeoutMs))
+         {
+            return 1;
+         }
+         return 0;
+      case CMD_NONE:
+         break;
+   }
+
+   printHelp();
    return 0;
 }
